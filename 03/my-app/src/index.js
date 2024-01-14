89e88a03f2d330ff77842f841ddd5225c40a37f8
axiaocainong <1443175752@qq.com>
2024-01-14 22:27:11 +0800

    feat:实现React.create函数

--- 03/my-app/src/index.js
+++ 03/my-app/src/index.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React from "./react";
 /* react18的写法
 import ReactDOM from "react-dom/client";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 let element = <div>Hello My App</div>;
 root.render(element); */
 
-import ReactDOM from "react-dom";
-ReactDOM.render(<div>Hello My App</div>, document.getElementById("root"));
-console.log(<div>Hello My App</div>);
+// import ReactDOM from "react-dom";
+// ReactDOM.render(<div>Hello My App</div>, document.getElementById("root"));
+console.log(
+  <div>
+    Hello My App<span>xxx1</span>
+    <span>xxx2</span>
+  </div>
+);
