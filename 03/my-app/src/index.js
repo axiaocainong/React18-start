89bb40cd0353e871c907b6b7ffbf16ce2f989375
axiaocainong <1443175752@qq.com>
2024-01-14 18:33:38 +0800

    附上

--- 03/my-app/src/index.js
+++ 03/my-app/src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom/client";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 let element = <div>Hello My App</div>;
 root.render(element); */
+
 import ReactDOM from "react-dom";
 ReactDOM.render(<div>Hello My App</div>, document.getElementById("root"));
 console.log(<div>Hello My App</div>);
