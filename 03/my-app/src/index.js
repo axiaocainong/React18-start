6826b713445028c0c8347a105e2267090c70d4a5
axiaocainong <1443175752@qq.com>
2024-01-15 11:28:36 +0800

    调试

--- 03/my-app/src/index.js
+++ 03/my-app/src/index.js
@@ -1,6 +1,9 @@
 import React from "./react";
 import ReactDOM from "./react-dom";
 ReactDOM.render(
-  <div style={{ color: "blue" }}>Hello My App</div>,
+  <div style={{ color: "blue" }}>
+    Hello My App<span>children1</span>
+    <span>children2</span>
+  </div>,
   document.getElementById("root")
 );
